a7c9ac6e9bfab8bfefca8cb1dbd06f44caebb52b
SpencerClark5 <spencerc5936@gmail.com>
2022-12-13 15:10:55 -0600

    finished game board]

--- game.py
+++ game.py
@@ -26,23 +26,35 @@ clock = pygame.time.Clock()
 
 #variables
 objects = []
+nonPhysicsObjects = []
+xOffset = 0
+yOffset = -75
 
 #create boards
-
 #left board
-#draws clock wise
-leftBoardTop = Polygon(window,local_points=[[0,0],[80,28],[96,0],[32/2,-28]],pos=Vector2(210,725),mass=math.inf,color=Vector3(255,0,0))
+leftBoardTop = Polygon(window,local_points=[[0,0],[80,28],[96,0],[32/2,-28]],pos=Vector2(210 +xOffset,727 - yOffset),mass=math.inf,color=Vector3(255,0,0))
 objects.append(leftBoardTop)
-
-leftBoardBottom = Polygon(window,local_points=[[0,0],[150,53],[165,28],[32/2,-28]],pos=Vector2(360,780),mass=math.inf,color=Vector3(255,0,0))
+leftBoardBottom = Polygon(window,local_points=[[0,0],[150,53],[165,28],[32/2,-28]],pos=Vector2(360 +xOffset,782 - yOffset),mass=math.inf,color=Vector3(255,0,0))
 objects.append(leftBoardBottom)
 
+#left board scoring zones
+#left3PointZone = Polygon(window,local_points=[[0,0],])
 
-#
 #right board
-rightBoardTop = Polygon(window,local_points=[[0,0],[80,28],[96,0],[16,-28]],pos=Vector2(500,725),mass=math.inf,color=Vector3(255,0,0))
+rightBoardTop = Polygon(window,local_points=[[0,0],[80,-28],[96,0],[32/2,28]],pos=Vector2(1610 -xOffset,722 - yOffset),mass=math.inf,color=Vector3(255,0,0))
 objects.append(rightBoardTop)
+rightBoardBottom = Polygon(window,local_points=[[0,0],[150,-53],[165,-28],[32/2,28]],pos=Vector2(1380 - xOffset,807 - yOffset),mass=math.inf,color=Vector3(255,0,0))
+objects.append(rightBoardBottom)
 
+#create floor
+floor = Wall(window, start_point=Vector2(0,910),end_point=(1920,910),color=Vector3(0,255,0))
+objects.append(floor)
+
+#create sticks
+leftStick = Polygon(window,local_points=[[0,0], [10,0],[10,105],[0,105]],color=(0,0,0),pos=Vector2(215 + xOffset,730 - yOffset),mass=math.inf)
+nonPhysicsObjects.append(leftStick)
+rightStick = Polygon(window,local_points=[[0,0], [10,0],[10,110],[0,110]],color=(0,0,0),pos=Vector2(1690 + xOffset,725 - yOffset),mass=math.inf)
+nonPhysicsObjects.append(rightStick)
 
 #game loop
 running = True
@@ -75,5 +87,7 @@ while running:
 
     # GRAPHICS
     # draw objects
+    for d in nonPhysicsObjects:
+        d.draw()
     for o in objects:
         o.draw()
